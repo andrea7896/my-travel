7ef361580cfb0c76814387cf606bde93e444f8f5
andrea7896 <andrea_nataly_05@hotmail.com>
2020-03-21 12:57:09 -0600

    Updates

---
+++ precache-manifest.7bb7f498f2beb942328053fbf68cb364.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e175142cff46f0ef82a9643544da6d7b",
+    "url": "/my-travel/index.html"
+  },
+  {
+    "revision": "41eb5f84bccfc60c9eaa",
+    "url": "/my-travel/static/css/2.72d9205d.chunk.css"
+  },
+  {
+    "revision": "96ee125d527f397b5993",
+    "url": "/my-travel/static/css/main.b3a430eb.chunk.css"
+  },
+  {
+    "revision": "41eb5f84bccfc60c9eaa",
+    "url": "/my-travel/static/js/2.cecad4c0.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/my-travel/static/js/2.cecad4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ee125d527f397b5993",
+    "url": "/my-travel/static/js/main.edcf41af.chunk.js"
+  },
+  {
+    "revision": "d0d1368328fa00a81cc1",
+    "url": "/my-travel/static/js/runtime-main.a7a0f863.js"
+  },
+  {
+    "revision": "44d090c8d58d4da141793d322bbc07fe",
+    "url": "/my-travel/static/media/adventure.44d090c8.jpg"
+  },
+  {
+    "revision": "1362a4af39674d87bb3d2dcd7c09db20",
+    "url": "/my-travel/static/media/couples.1362a4af.jpg"
+  },
+  {
+    "revision": "bab76368f08dd83cd0b9f1d420177c26",
+    "url": "/my-travel/static/media/friends.bab76368.jpg"
+  },
+  {
+    "revision": "7b181d6027f9ac3f1225c486076ba727",
+    "url": "/my-travel/static/media/fuji.7b181d60.jpg"
+  },
+  {
+    "revision": "2c68d4671d067f498da93193e1d7e07c",
+    "url": "/my-travel/static/media/giphy.2c68d467.gif"
+  },
+  {
+    "revision": "0d1d5bece652921b90361505af5c81ae",
+    "url": "/my-travel/static/media/girl.0d1d5bec.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-travel/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "44654d6ea2e7edbc50ad88e201599063",
+    "url": "/my-travel/static/media/mountain2.44654d6e.png"
+  }
+]);
